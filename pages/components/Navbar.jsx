c831552fd48131c7b7d6f0e63f1cfdf64c5d7291
Alex-Hemming <72031268+Alex-Hemming@users.noreply.github.com>
2021-09-10 10:56:00 +0100

    feat(WEB-01):first setup

---
+++ pages/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import styles from './Navbar.module.css'
+import Link from "next/link"
+import Image from 'next/image'
+
+function Navbar() {
+    return (<nav className={styles.container}>
+                    <Image src="/Logo-blue.svg" className={styles.logo} alt="vape Logo" width={500} height={50} />
+                    <i className="fa fa-bars"></i>
+                    <i className="fa fa-bars"></i>
+ <div className={styles.navbar}>
+<Link href="/about">Abotu Us</Link>
+<Link href="/about">Creators</Link>
+<Link href="/about">How It Works</Link>
+<Link href="/about">Learn More</Link>
+<Link href="/about">Login</Link>
+<button className={styles.button}>Start Learning today</button>
+      </div>
+      </nav>
+    )
+}
+
+export default Navbar
---
+++ pages/components/carousel.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import styles from "./carousel.module.css";
+
+
+function Carousel() {
+    return (
+        <div className={styles.carousel}>
+        <h1 className={styles.title}>
+            Create. SUBMIT. Earn!
+            </h1>
+            <div className={styles.description}>
+            Tap into the power of your creative customers 
+            for branded content and authentic word-of-mouth influencer marketing.
+            </div>
+            <div className={styles.left}>
+            Unified by social content creation!
+            </div>
+            <div className={styles.right}>
+            <div className={styles.Ocircle}>
+                £££</div>
+            <div className={styles.Wcircle}>200+</div>
+
+            £1000’s earned by creators every day!
+        </div>
+            <div>
+
+            Campaigns available to make you money!
+        </div>
+            </div>
+
+
+
+)
+}
+
+export default Carousel
